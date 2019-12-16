5d8339c942302cf9baffd0378eb7c14ac44bb90e
ali.azaz <ali.azaz.alam@hotmail.com>
2019-12-16 17:56:36 +0500

    working on face detection classes

---
+++ app/src/main/java/com/abcd/firebasemlkit02/ui/ImageParseActivity.kt
@@ -0,0 +1,13 @@
+package com.abcd.firebasemlkit02.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.abcd.firebasemlkit02.R
+
+class ImageParseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_parse)
+    }
+}
